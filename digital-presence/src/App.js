c4d7098cc309de05558a985612a1d25d0ce7656e
malka-308 <mat33308@gmail.com>
2022-05-22 23:39:24 +0300

    (feat)add update child details page

--- digital-presence/src/App.js
+++ digital-presence/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-import AddNewChild from './addNewChild';
+import UpdateChild from './updataChild';
 
 function App() {
   return (<>
-  <AddNewChild/>
+  <UpdateChild/>
   </>);
 }
 
