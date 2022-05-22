c9e1c1d5fc31fab161cad79a5a5a2173b53f4080
rachel-2863 <rachelz2863@gmail.com>
2022-05-22 15:16:22 +0300

    add teamMember page

--- digital-presence/src/App.js
+++ digital-presence/src/App.js
@@ -3,15 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './login';
 import PresencePage from './presencePage';
+import AddTeamMember from './addTeamMember';
 
 
 function App() {
 
 
   return (<>
-
+    <AddTeamMember />
   </>);
- 
+
 }
 
 
